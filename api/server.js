87ed974ad85ae48305db6eb5d4ebc830e5cb4ef6
Jazer <contact@jazer.co.uk>
2021-02-04 08:58:55 +0000

    feat: added middlware

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require('helmet')
 
 const app = express()
 app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send('It Works')
