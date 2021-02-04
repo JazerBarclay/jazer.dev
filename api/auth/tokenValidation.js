db5273f4691e797773e0b9eb9e00b9c3604ee97e
Jazer <contact@jazer.co.uk>
2021-02-04 16:27:04 +0000

    feat: added token validation using jwt

---
+++ api/auth/tokenValidation.js
@@ -0,0 +1,15 @@
+const { verify } = require('jsonwebtoken')
+
+module.exports = {
+    validate: (req, res, next) => {
+        let token = req.get('authorization')
+        if (!token) return res.status(403).json({success: 0, message: "Access Denied! Unauthorised User"})
+        // Remove 'Bearer ' string before key
+        token = token.slice(7)
+        verify(token, process.env.JWT_KEY, (err, decoded) => {
+            if (err) return res.status(403).json({success: 0, message: "Access Denied! Invalid Token"})
+            console.log(decoded)
+            next()
+        })
+    }
+}
