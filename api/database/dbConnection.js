89f17e997a7ac9484a681653f0fc3aa414ffa498
Jazer <contact@jazer.co.uk>
2021-02-04 09:30:36 +0000

    feat: pg database connection

---
+++ api/database/dbConnection.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg')
+
+console.log(`Connected to ${process.env.DATABASE_URL}`)
+
+const databaseConfig = { connectionString: process.env.DATABASE_URL }
+const pool = new Pool(databaseConfig)
+
+pool.on('connect', () => {
+    console.log('Connected to the db')
+})
+
+module.exports = pool
