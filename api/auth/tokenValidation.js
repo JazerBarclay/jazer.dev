5e462e5d69cf42152091e0caa0cbea6cc6da672a
Jazer <contact@jazer.co.uk>
2021-02-07 16:02:46 +0000

    refactor: no longer returns forbidden if no token provided

--- api/auth/tokenValidation.js
+++ api/auth/tokenValidation.js
@@ -3,13 +3,21 @@ const { verify } = require('jsonwebtoken')
 module.exports = {
     validate: (req, res, next) => {
         let token = req.get('authorization')
-        if (!token) return res.status(403).json({message: "Access Denied! Unauthorised User"})
+        if (!token) {
+            req.body.auth = {
+                user_id: 0,
+                username: 'guest',
+                verified: false
+            }
+            return next()
+        }
+        
         // Remove 'Bearer ' string before key
         token = token.slice(7)
         verify(token, process.env.JWT_KEY, (err, decoded) => {
-            if (err) return res.status(403).json({message: "Access Denied! Invalid Token"})
-            console.log(decoded)
-            next()
+            if (err) return res.status(403).json({message: "Error - Invalid Token"})
+            req.body.auth = decoded.result
+            return next()
         })
     }
 }
