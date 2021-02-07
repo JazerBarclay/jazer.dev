b8b87d88624afab5f1eb28f51929a475286d4611
Jazer <contact@jazer.co.uk>
2021-02-07 09:07:56 +0000

    refactor: removed redundant success code

--- api/auth/tokenValidation.js
+++ api/auth/tokenValidation.js
@@ -3,11 +3,11 @@ const { verify } = require('jsonwebtoken')
 module.exports = {
     validate: (req, res, next) => {
         let token = req.get('authorization')
-        if (!token) return res.status(403).json({success: 0, message: "Access Denied! Unauthorised User"})
+        if (!token) return res.status(403).json({message: "Access Denied! Unauthorised User"})
         // Remove 'Bearer ' string before key
         token = token.slice(7)
         verify(token, process.env.JWT_KEY, (err, decoded) => {
-            if (err) return res.status(403).json({success: 0, message: "Access Denied! Invalid Token"})
+            if (err) return res.status(403).json({message: "Access Denied! Invalid Token"})
             console.log(decoded)
             next()
         })
