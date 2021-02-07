0aa7cfade9636289e1e14e573ca51162d3f77d33
Jazer <contact@jazer.co.uk>
2021-02-07 16:03:52 +0000

    refactor: altered seeding for admin to some default values

--- api/database/seed.sql
+++ api/database/seed.sql
@@ -1,16 +1,17 @@
+-- INIT IMAGE TYPES
 INSERT INTO image_types (
     image_type
 ) VALUES
 ('PNG'), ('JPG'), ('GIF'), ('SVG'), ('RAW');
 
+-- CREATE ADMIN USER
 INSERT INTO users (
 	email, username, password, verified
 ) VALUES (
-	'admin@jazer.dev', 'admin', 'admin', true
-), (
-	'jazer@jazer.dev', 'jazer', 'dev', false
+	'admin@jazer.dev', 'jazer', '$2b$10$dxS6ZCKpwR5aYJ19dEKC8.DSdvrJTPj3ASq.1lx9a61CtJkZio9UK', true
 );
 
+-- CREATE DUMMY POST
 INSERT INTO posts (
 	author, slug, title, body
 ) VALUES (
