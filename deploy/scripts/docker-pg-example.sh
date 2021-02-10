f3587f5eadacd744325c624fc5ce0504efcbb4a1
Jazer <contact@jazer.co.uk>
2021-02-10 08:35:53 +0000

    feat: modified script to generate user and db automatically

--- deploy/scripts/docker-pg-example.sh
+++ deploy/scripts/docker-pg-example.sh
@@ -5,4 +5,4 @@ docker stop blogdb 2>/dev/null
 docker rm blogdb 2>/dev/null
 
 # Run new container under blogdb
-docker run -p 5432:5432 --name blogdb -e POSTGRES_PASSWORD=pgpassword -d postgres
+docker run -p 5432:5432 --name blogdb -e POSTGRES_USER=web -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=blog -d postgres
