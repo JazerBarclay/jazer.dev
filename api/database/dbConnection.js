f5fcea81d2b1576c3269028c1d7f11875e41bc95
Jazer <contact@jazer.co.uk>
2021-02-24 19:48:29 +0000

    refactor: removed pool connection log for production

--- api/database/dbConnection.js
+++ api/database/dbConnection.js
@@ -5,8 +5,8 @@ console.log(`Connected to ${process.env.DATABASE_URL}`)
 const databaseConfig = { connectionString: process.env.DATABASE_URL }
 const pool = new Pool(databaseConfig)
 
-pool.on('connect', () => {
-    console.log('Connected to the db')
-})
+// pool.on('connect', () => {
+//     console.log('Connected to the db')
+// })
 
 module.exports = pool
